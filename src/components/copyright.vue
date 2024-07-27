054bf6cfd619a804f2b9a5cdbcec8bf07ee5364b
Nitai_9h <108105942+Nitai9h@users.noreply.github.com>
2024-07-28 02:33:36 +0800

    Update copyright.vue

--- src/components/copyright.vue
+++ src/components/copyright.vue
@@ -19,7 +19,7 @@ import * as pkg from "../../package.json";
       <a class="copyright-p-1-1" href="https://sponsors.yunyoujun.cn" target="_blank" rel="noreferrer">
         <div class="copyright-p-1-1-icon" />
       </a>
-      <a :href="pkg.author.url" target="_blank" rel="noreferrer">Lucky薄荷&Nitai_ @{{ pkg.author.name }}</a>
+      <a :href="pkg.author.url" target="_blank" rel="noreferrer">便携小空调 @{{ pkg.author.name }}</a>
     </div>
     <p>2024 - {{ new Date().getFullYear() }}</p>
   </div>
